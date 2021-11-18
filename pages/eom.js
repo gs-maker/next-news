33f914a4c60f9424dbca1c8574b4f752ca043ba9
gs-maker <gerald.ssali@gmail.com>
2021-11-18 21:06:36 +0300

    seo tags

--- pages/eom.js
+++ pages/eom.js
@@ -1,22 +1,38 @@
+import Head from "next/head";
 import styles from "../styles/EOM.module.css";
 import Toolbar from "../components/toolbar";
 
 export const EOM = ({ employee }) => {
 	// console.log(employee);
 	return (
-		<div className="page-container">
-			<Toolbar />
-			<div className={styles.main}>
-				<h1>Employees of the month</h1>
+		<>
+			<Head>
+				<title>Employee Of The Month</title>
+				<meta name="description" content={`This month's employee of the month is ${employee.name}`} />
 
-				<div className={styles.employeeOfTheMonth}>
-					<h3>{employee.name}</h3>
-					<h6>{employee.location}</h6>
-					<img src={employee.avatar_url} alt={employee.name} />
-					<p>{employee.bio}</p>
+				<meta property="og:image" content={employee.image} />
+				<meta property="og:title" content="Employee Of The Month" />
+				<meta property="og:description" content={`This month's employee of the month is ${employee.name}`} />
+
+				<meta property="twitter:image" content={employee.image} />
+				<meta property="twitter:title" content="Employee Of The Month" />
+				<meta property="twitter:description" content={`This month's employee of the month is ${employee.name}`} />
+			</Head>
+			
+			<div className="page-container">
+				<Toolbar />
+				<div className={styles.main}>
+					<h1>Employees of the month</h1>
+
+					<div className={styles.employeeOfTheMonth}>
+						<h3>{employee.name}</h3>
+						<h6>{employee.location}</h6>
+						<img src={employee.avatar_url} alt={employee.name} />
+						<p>{employee.bio}</p>
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
--- pages/feed/[slug].js
+++ pages/feed/[slug].js
@@ -1,46 +1,62 @@
+import Head from "next/head";
 import styles from "../../styles/Feed.module.css";
 import Toolbar from "../../components/toolbar";
 import { useRouter } from "next/router";
 
 function Feed({ articles, pageNumber }) {
 	const router = useRouter();
-	return (
+	return articles.length ? (
+		<>
+			<Head>
+				<meta property="og:image" content={articles[0]?.urlToImage} />
+				<meta property="og:description" content={articles[0]?.description} />
+				<meta property="og:title" content={articles[0]?.title + " and more!"} />
+			</Head>
+            
+			<div className="page-container">
+				<Toolbar />
+				<div className={styles.main}>
+					{articles.map((article, index) => {
+						return (
+							<div key={index} className={styles.post}>
+								<h1 onClick={() => (window.location.href = article.url)}>{article.title}</h1>
+								<p>{article.description}</p>
+								{!!article.urlToImage && <img src={article.urlToImage} />}
+							</div>
+						);
+					})}
+				</div>
+
+				<div className={styles.paginator}>
+					<div
+						onClick={() => {
+							if (pageNumber > 1) {
+								router.push(`/feed/${pageNumber - 1}`).then(() => window.scrollTo(0, 0));
+							}
+						}}
+						className={pageNumber === 1 ? styles.disabled : styles.active}
+					>
+						Previous Page
+					</div>
+					<div>#{pageNumber}</div>
+					<div
+						onClick={() => {
+							if (pageNumber < 5) {
+								router.push(`/feed/${pageNumber + 1}`).then(() => window.scrollTo(0, 0));
+							}
+						}}
+						className={pageNumber === 5 ? styles.disabled : styles.active}
+					>
+						Next Page
+					</div>
+				</div>
+			</div>
+		</>
+	) : (
 		<div className="page-container">
 			<Toolbar />
 			<div className={styles.main}>
-				{articles.map((article, index) => {
-					return (
-						<div key={index} className={styles.post}>
-							<h1 onClick={() => (window.location.href = article.url)}>{article.title}</h1>
-							<p>{article.description}</p>
-							{!!article.urlToImage && <img src={article.urlToImage} />}
-						</div>
-					);
-				})}
-			</div>
-
-			<div className={styles.paginator}>
-				<div
-					onClick={() => {
-						if (pageNumber > 1) {
-							router.push(`/feed/${pageNumber - 1}`).then(() => window.scrollTo(0, 0));
-						}
-					}}
-					className={pageNumber === 1 ? styles.disabled : styles.active}
-				>
-					Previous Page
-				</div>
-				<div>#{pageNumber}</div>
-				<div
-					onClick={() => {
-						if (pageNumber < 5) {
-							router.push(`/feed/${pageNumber + 1}`).then(() => window.scrollTo(0, 0));
-						}
-					}}
-					className={pageNumber === 5 ? styles.disabled : styles.active}
-				>
-					Next Page
-				</div>
+				<h1>Oops! No articles for this page</h1>
 			</div>
 		</div>
 	);
